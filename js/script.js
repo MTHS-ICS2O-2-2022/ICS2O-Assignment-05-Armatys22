38404c1a64c185ef9d8b8a4406a3d89f36bedc52
Armatys22 <124389724+Armatys22@users.noreply.github.com>
2023-05-30 23:21:30 +0000

    gett

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dominik Armatys
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict";
+
+function calculate() {
+  var input = document.getElementById("input").value;
+  var number = parseInt(input);
+
+  var digitCount = 0;
+  while (number !== 0) {
+    number = Math.floor(number / 10);
+    digitCount++;
+  }
+
+  document.getElementById("output").innerHTML = "Total number of digits: " + digitCount;
+}
